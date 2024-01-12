c10788f67f9354bbfd7cdc4ef115041f780efa04
Gary-Starling <skvorec92@mail.ru>
2024-01-12 11:02:02 +0300

    parent_child_pipe

--- process/child_inherits/main.c
+++ process/child_inherits/main.c
@@ -3,12 +3,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-const char str[] = "Share msg";
 
 int main(int argc, char const *argv[])
 {
     pid_t pid;
-    int cnt;
+    int cnt, c_cnt = 0;
+    /*  r[0] w[1]           r[0] w[1]*/
     int pipe_to_par[2], pipe_to_chil[2];
     char buf[256] = {0};
 
@@ -28,18 +28,23 @@ int main(int argc, char const *argv[])
 
     if (pid == 0) /* children */
     {
-        close(0);             // close stdin
-        dup(pipe_to_chil[0]); // copy fd read
-        close(1);             // close stdout
-        dup(pipe_to_par[0]);
+        close(0);               // close stdin
+        dup(pipe_to_chil[0]);   // copy fdr
+        close(1);               // close stdout
+        dup(pipe_to_par[1]);    // copy fdw
         close(pipe_to_par[1]);  // close fd wr pipe
         close(pipe_to_chil[0]); // close fd r pipe
-        close(pipe_to_par[0]);
-        close(pipe_to_chil[1]);
+        close(pipe_to_par[0]);  // ,,
+        close(pipe_to_chil[1]); // ..
 
-        if ((count = read(0, buf, sizeof(buf)) == 0))
-            exit();
-        write(1, buff, count);
+        while (1)
+        {
+            c_cnt++;
+            if ((cnt = read(0, buf, sizeof(buf))) == 0)
+                exit(EXIT_SUCCESS);
+            sprintf(buf, "children recived msg %d, response ->", c_cnt);
+            write(1, buf, strlen(buf));
+        }
     }
     else if (pid < 0) /* error */
     {
@@ -48,20 +53,21 @@ int main(int argc, char const *argv[])
     }
     else /* parent */
     {
-        close(1);             // close stdout
-        dup(pipe_to_chil[1]); //
-        close(0);             // close stdin
-        dup(pipe_to_par[1]);
-        close(pipe_to_par[1]);  // close fd wr pipe
-        close(pipe_to_chil[0]); // close fd r pipe
-        close(pipe_to_par[0]);
-        close(pipe_to_chil[1]);
+        close(1);               // close stdout
+        dup(pipe_to_chil[1]);   // copy fd(w)
+        close(0);               // close stdin
+        dup(pipe_to_par[0]);    // copy fd(r)
+        close(pipe_to_par[1]);  // close fd r pipe
+        close(pipe_to_chil[1]); // close fd w pipe
+        close(pipe_to_chil[0]); // ..
+        close(pipe_to_par[0]);  // ..
     }
 
-    for(unsigned int i = 0; i < 15; i++)
+    for (unsigned int i = 0; i < 15; i++)
     {
-        write(1, str, strlen(str));
-        read(0, buf, sizeof(buff));
+        sprintf(buf, "Paren sended msg %d ->", i);
+        write(1, buf, strlen(buf));
+        read(0, buf, sizeof(buf));
     }
 
     return 0;
