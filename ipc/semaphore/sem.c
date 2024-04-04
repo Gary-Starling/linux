9cfd759fe913fa7899bb531c43e71a54aff16f7d
Gary-Starling <skvorec92@mail.ru>
2024-04-04 16:44:44 +0300

    semaphore

--- ipc/semaphore/sem.c
+++ ipc/semaphore/sem.c
@@ -1,56 +1,69 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/sem.h>
+#include <sys/wait.h>
 
-
-union senum {
+union senum
+{
     int val;
     struct semid_ds *buf;
-    unsigned short * arrau;
+    unsigned short *arrau;
     struct seminfo *__buf;
-}
+};
 
 union senum s;
 
-struct sembuf sem1 = {0, 1, SEM_UNDO};
-struct sembuf sem2 = {0, -1, SEM_UNDO};
+struct sembuf sem_send = {0, 1, SEM_UNDO};
+struct sembuf sem_recv = {0, -1, SEM_UNDO};
 
 int main(int argc, char const *argv[])
 {
-    const char * send_str = "Get semaphore";
+
     pid_t pid;
     int id = semget(0xfafa, 1, 0666 | IPC_CREAT);
 
-    if(id < 0)
+    if (id < 0)
     {
         printf("Error semger\n");
         return 1;
     }
 
-    s.val = 1;
+    s.val = 10;
 
-    if(semctl(id, 0, SETVAL, s) < 0)
+    if (semctl(id, 0, SETVAL, s) < 0)
     {
         printf("Error semctl");
         return 1;
     }
 
-    if((pid=fork()) < 0)
+    if ((pid = fork()) < 0)
     {
         printf("Error fork");
         return 1;
     }
 
-    if(pid == 0)
+    if (pid == 0)
     {
+        int n = 1;
+        int semv;
 
+        while (n < 21)
+        {
+           semv = semctl(id, 0, GETVAL, 0); 
+           semop(id, &sem_recv, 1); 
+           printf("get task %d : value = %d\n", n, semv);
+           n++;
+        }
     }
     else
     {
-        semop(id, &sem1, 1);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            semop(id, &sem_send, 1);
+            sleep(1);
+        }
+        wait(NULL);
     }
-
-
 
     return 0;
 }
